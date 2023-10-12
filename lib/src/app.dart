97421df0b4483ba38f44b85390690d1d3f01aa3c
Pavel Yadlouski <pavel.yadlouski@veeam.com>
2023-10-12 16:52:05 +0200

    Add setting control

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,6 +11,13 @@ import 'package:provider/provider.dart';
 
 
 class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+    required this.settingsController,
+  }) : super(key: key);
+  
+  final SettingsController settingsController;
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -103,108 +110,86 @@ class ProfileScreen extends StatelessWidget {
   }
 }
 
-// class _HomePageState extends State<HomePage>{
-//   @override
-//   var selectedIndex = 0;
-//   Widget build(BuildContext context) {
-//     Widget page;
-//     switch (selectedIndex) {
-//       case 0:
-//         page = GeneratorPage();
-//         break;
-//       case 1:
-//         page = FavoritesPage();
-//         break;
-//       default:
-//         throw UnimplementedError('no widget for $selectedIndex');
-//     }
+// The Widget that configures your application.
+class MyApp1 extends StatelessWidget {
+  const MyApp1({
+    super.key,
+    required this.settingsController,
+  });
 
-//     return Scaffold(body: Row,)
+  final SettingsController settingsController;
 
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+        AppBar(
+          title: const Text("Simple moments"),
+        )
 
-// }
-
-/// The Widget that configures your application.
-// class MyApp extends StatelessWidget {
-//   const MyApp({
-//     super.key,
-//     required this.settingsController,
-//   });
-
-//   final SettingsController settingsController;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar:
-//         AppBar(
-//           title: const Text("Simple moments"),
-//         )
-
-//     );
+    );
     // Glue the SettingsController to the MaterialApp.
-    //
+    
     // The ListenableBuilder Widget listens to the SettingsController for changes.
     // Whenever the user updates their settings, the MaterialApp is rebuilt.
-  //   return ListenableBuilder(
-  //     listenable: settingsController,
-  //     builder: (BuildContext context, Widget? child) {
-  //       return MaterialApp(
-  //         // Providing a restorationScopeId allows the Navigator built by the
-  //         // MaterialApp to restore the navigation stack when a user leaves and
-  //         // returns to the app after it has been killed while running in the
-  //         // background.
-  //         restorationScopeId: 'app',
+    return ListenableBuilder(
+      listenable: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          // Providing a restorationScopeId allows the Navigator built by the
+          // MaterialApp to restore the navigation stack when a user leaves and
+          // returns to the app after it has been killed while running in the
+          // background.
+          restorationScopeId: 'app',
 
-  //         // Provide the generated AppLocalizations to the MaterialApp. This
-  //         // allows descendant Widgets to display the correct translations
-  //         // depending on the user's locale.
-  //         localizationsDelegates: const [
-  //           AppLocalizations.delegate,
-  //           GlobalMaterialLocalizations.delegate,
-  //           GlobalWidgetsLocalizations.delegate,
-  //           GlobalCupertinoLocalizations.delegate,
-  //         ],
-  //         supportedLocales: const [
-  //           Locale('en', ''), // English, no country code
-  //         ],
+          // Provide the generated AppLocalizations to the MaterialApp. This
+          // allows descendant Widgets to display the correct translations
+          // depending on the user's locale.
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', ''), // English, no country code
+          ],
 
-  //         // Use AppLocalizations to configure the correct application title
-  //         // depending on the user's locale.
-  //         //
-  //         // The appTitle is defined in .arb files found in the localization
-  //         // directory.
-  //         onGenerateTitle: (BuildContext context) =>
-  //             AppLocalizations.of(context)!.appTitle,
+          // Use AppLocalizations to configure the correct application title
+          // depending on the user's locale.
+          //
+          // The appTitle is defined in .arb files found in the localization
+          // directory.
+          onGenerateTitle: (BuildContext context) =>
+              AppLocalizations.of(context)!.appTitle,
 
-  //         // Define a light and dark color theme. Then, read the user's
-  //         // preferred ThemeMode (light, dark, or system default) from the
-  //         // SettingsController to display the correct theme.
-  //         theme: ThemeData(),
-  //         darkTheme: ThemeData.dark(),
-  //         themeMode: settingsController.themeMode,
+          // Define a light and dark color theme. Then, read the user's
+          // preferred ThemeMode (light, dark, or system default) from the
+          // SettingsController to display the correct theme.
+          theme: ThemeData(),
+          darkTheme: ThemeData.dark(),
+          themeMode: settingsController.themeMode,
 
-  //         // Define a function to handle named routes in order to support
-  //         // Flutter web url navigation and deep linking.
-  //         onGenerateRoute: (RouteSettings routeSettings) {
-  //           return MaterialPageRoute<void>(
-  //             settings: routeSettings,
-  //             builder: (BuildContext context) {
-  //               switch (routeSettings.name) {
-  //                 case SettingsView.routeName:
-  //                   return SettingsView(controller: settingsController);
-  //                 case SampleItemDetailsView.routeName:
-  //                   return const SampleItemDetailsView();
-  //                 case SampleItemListView.routeName:
-  //                 default:
-  //                   return const SampleItemListView();
-  //               }
-  //             },
-  //           );
-  //         },
-  //       );
-  //     },
-  //   );
-//   }
-// }
+          // Define a function to handle named routes in order to support
+          // Flutter web url navigation and deep linking.
+          onGenerateRoute: (RouteSettings routeSettings) {
+            return MaterialPageRoute<void>(
+              settings: routeSettings,
+              builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case SettingsView.routeName:
+                    return SettingsView(controller: settingsController);
+                  case SampleItemDetailsView.routeName:
+                    return const SampleItemDetailsView();
+                  case SampleItemListView.routeName:
+                  default:
+                    return const SampleItemListView();
+                }
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
